0268eb6eb160be477fc3cd14f1f7ef8b6bb19fc4
Shape <shapeintheglass@gmail.com>
2022-05-14 12:28:08 -0700

    Row operations

--- visualizer/parser.js
+++ visualizer/parser.js
@@ -3,6 +3,7 @@
 var jsonObj;
 var subchunkIndex;
 
+// Shamelessly copied from stackoverflow, determines if a string is valid JSON
 function isJsonString(str) {
   try {
     JSON.parse(str);
@@ -12,34 +13,105 @@ function isJsonString(str) {
   return true;
 }
 
-
+// Restores cached state on load
 window.onload = function () {
   var cached = localStorage.getItem("jsonObj");
   if (cached != null) {
     document.getElementById("textarea").value = cached;
+  }
+  var cachedSubchunkIndex = localStorage.getItem("subchunkIndex");
+  if (cachedSubchunkIndex != null) {
+    subchunkIndex = cachedSubchunkIndex;
   }
   if (isJsonString(cached)) {
     populateSelector();
   }
 }
 
+// Inserts a special cell that duplicates the current row when clicked
+function insertDuplicateCell(row, lineIndex) {
+  let cell = row.insertCell();
+  cell.setAttribute("class", "mdc-data-table__cell");
+  cell.innerHTML = "Duplicate";
+  cell.subchunkIndex = subchunkIndex;
+  cell.lineIndex = lineIndex;
+  cell.addEventListener('click', duplicateRowListener);
+}
+
+function duplicateRowListener() {
+  console.log(`duplicating line at subchunk index ${this.subchunkIndex}, line ${this.lineIndex}`);
+  var currLine = jsonObj.SubChunks[this.subchunkIndex].Lines[this.lineIndex];
+  jsonObj.SubChunks[this.subchunkIndex].Lines.splice(this.lineIndex, 0, currLine);
+  populateTable();
+}
+
+
+// Inserts a special cell that inserts a new row when clicked
+function insertAddAboveCell(row, lineIndex) {
+  let cell = row.insertCell();
+  cell.setAttribute("class", "mdc-data-table__cell");
+  cell.innerHTML = "Insert Above";
+  cell.subchunkIndex = subchunkIndex;
+  cell.lineIndex = lineIndex;
+  cell.addEventListener('click', addAboveRowListener);
+}
+
+function addAboveRowListener() {
+  console.log(`adding line at subchunk index ${this.subchunkIndex}, line ${this.lineIndex}`);
+  jsonObj.SubChunks[this.subchunkIndex].Lines.splice(this.lineIndex, 0, {});
+  populateTable();
+}
+
+// Inserts a special cell that inserts a new row when clicked
+function insertAddBelowCell(row, lineIndex) {
+  let cell = row.insertCell();
+  cell.setAttribute("class", "mdc-data-table__cell");
+  cell.innerHTML = "Insert Below";
+  cell.subchunkIndex = subchunkIndex;
+  cell.lineIndex = lineIndex;
+  cell.addEventListener('click', addBelowRowListener);
+}
+
+function addBelowRowListener() {
+  console.log(`adding line at subchunk index ${this.subchunkIndex}, line ${this.lineIndex}`);
+  jsonObj.SubChunks[this.subchunkIndex].Lines.splice(this.lineIndex + 1, 0, {});
+  populateTable();
+}
+
+// Inserts a special cell that removes the current row when clicked
+function insertRemoveCell(row, lineIndex) {
+  let cell = row.insertCell();
+  cell.setAttribute("class", "mdc-data-table__cell");
+  cell.innerHTML = "Remove";
+  cell.subchunkIndex = subchunkIndex;
+  cell.lineIndex = lineIndex;
+  cell.addEventListener('click', removeRowListener);
+}
+
+// Listener for removing a cell
+function removeRowListener() {
+  console.log(`removing line at subchunk index ${this.subchunkIndex}, line ${this.lineIndex}`);
+  jsonObj.SubChunks[this.subchunkIndex].Lines.splice(this.lineIndex, 1);
+  populateTable();
+}
+
+// Inserts an editable cell
 function insertCell(row, lineIndex, fieldName) {
   let cell = row.insertCell();
   cell.setAttribute("class", "mdc-data-table__cell");
   const input = document.createElement("input");
-  if (lineIndex == null) {
-    input.value = jsonObj.SubChunks[subchunkIndex].Name;
-  }
-  else {
-    input.value = jsonObj.SubChunks[subchunkIndex].Lines[lineIndex][fieldName];
-  }
+  var value = jsonObj.SubChunks[subchunkIndex].Lines[lineIndex][fieldName];
+  input.value = value == undefined ? "" : value;
 
   input.type = "text";
   input.subchunkIndex = subchunkIndex;
   input.lineIndex = lineIndex;
   input.fieldName = fieldName;
-  if (fieldName == "Evt") {
+  if (fieldName == "Evt" || fieldName == "Trgt" || fieldName == "Spkr") {
     input.style = "width:300px";
+  }
+  if (fieldName == "Loc") {
+    input.style = "width:25px";
   }
   if (fieldName == "Txt" || fieldName == "Cmt") {
     input.style = "width:1000px";
@@ -48,32 +120,31 @@ function insertCell(row, lineIndex, fieldName) {
   cell.appendChild(input);
 }
 
+// Listener for updating the underlying json field when an editable cell is changed
 function updateJsonListener() {
-  console.log(this.value);
-  console.log(this.subchunkIndex);
-  console.log(this.lineIndex);
-  console.log(this.fieldName);
-  if (this.fieldName == null) {
-    jsonObj.SubChunks[this.subchunkIndex].Name = this.value;
+  console.log(`updating subchunk index ${this.subchunkIndex}, line index ${this.lineIndex}, field name ${this.fieldName} to ${this.value}`);
+  // Delete the field from the JSON if the cell is cleared
+  if (this.value == "" || this.value == "undefined") {
+    delete jsonObj.SubChunks[this.subchunkIndex].Lines[this.lineIndex][this.fieldName];
   }
   else {
-    if (this.value == "" || this.value == "undefined") {
-      delete jsonObj.SubChunks[this.subchunkIndex].Lines[this.lineIndex][this.fieldName];
-    }
-    else {
-      jsonObj.SubChunks[this.subchunkIndex].Lines[this.lineIndex][this.fieldName] = this.value;
-    }
-
+    jsonObj.SubChunks[this.subchunkIndex].Lines[this.lineIndex][this.fieldName] = this.value;
   }
 }
 
+// Listener for updating the cache every time the textarea is changed
 function textareaListener() {
   console.log("updating cache")
   var jsonInput = document.getElementById("textarea").value;
   localStorage.setItem("jsonObj", jsonInput);
 }
 
+// Populates a row
 function insertRow(row, lineIndex) {
+  insertDuplicateCell(row, lineIndex);
+  insertAddAboveCell(row, lineIndex);
+  insertAddBelowCell(row, lineIndex);
+  insertRemoveCell(row, lineIndex);
   insertCell(row, lineIndex, "Evt");
   insertCell(row, lineIndex, "Txt");
   insertCell(row, lineIndex, "Spkr");
@@ -85,12 +156,14 @@ function insertRow(row, lineIndex) {
   insertCell(row, lineIndex, "Loc");
 }
 
+// Loads JSON from the textarea and parses it into an object
 function loadJsonObj() {
   var jsonInput = document.getElementById("textarea").value;
   jsonObj = JSON.parse(jsonInput);
-  console.log(jsonObj);
+  console.log("loaded json");
 }
 
+// Populates the subchunk dropdown selector
 function populateSelector() {
   loadJsonObj();
   var selector = document.getElementById("subchunkSelector");
@@ -102,16 +175,21 @@ function populateSelector() {
     option.text = subchunk.Name;
     selector.add(option);
   }
+  if (subchunkIndex != undefined) {
+    selector.selectedIndex = subchunkIndex;
+  }
   selectorListener();
-
 }
 
+// Listener updating the current global subchunk index and cached subchunk index every time the selector is updated
 function selectorListener() {
   var selector = document.getElementById("subchunkSelector");
   subchunkIndex = selector.selectedIndex;
+  localStorage.setItem("subchunkIndex", subchunkIndex);
   populateTable();
 }
 
+// Populates the table with what is currently in the global jsonObj variable
 function populateTable() {
   const table = document.getElementById("datatablebody");
   table.innerHTML = "";
@@ -127,8 +205,8 @@ function populateTable() {
 
 }
 
+// Populates the text area with what is currenly in the global jsonObj variable
 function populateTextArea() {
-  console.log(jsonObj);
   var textbox = document.getElementById("textarea");
   var textToSet = JSON.stringify(jsonObj, null, 2)
   textbox.value = textToSet;

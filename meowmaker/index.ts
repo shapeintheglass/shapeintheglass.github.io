59f353e0277c493c1ae9e5ac02b1f7de89bab806
Shape <shapeintheglass@gmail.com>
2023-08-05 17:32:29 -0700

    Fix drawing

--- meowmaker/index.ts
+++ meowmaker/index.ts
@@ -1,21 +1,28 @@
 import Perspective from "perspectivets"
 
-const canvas = document.getElementById("meow");
-const meowBase = document.querySelector("img");
+async function drawBase(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, imgSrc: string) {
+  const meowBase = new Image();
+  meowBase.src = imgSrc
 
-if (canvas instanceof HTMLCanvasElement) {
-  const ctx = canvas.getContext("2d");
-  if (ctx && meowBase) {
-    let p = new Perspective(ctx, meowBase)
-    p.draw({
-      topLeftX: 30,
-      topLeftY: 30,
-      topRightX: 462,
-      topRightY: 50,
-      bottomRightX: 442,
-      bottomRightY: 482,
-      bottomLeftX: 10,
-      bottomLeftY: 512,
-    })
-  }
+  return new Promise((resolve, reject) => {
+    meowBase.onload = () => {
+      ctx.drawImage(meowBase, 0, 0);
+      resolve(true);
+    }
+    meowBase.onerror = reject;
+  });
 }
+
+async function main() {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 512;
+  canvas.height = 512;
+  await drawBase(canvas, ctx, "./meow_bongo_base.png");
+  await drawBase(canvas, ctx, "./meow_bongo_lower_left.png");
+  await drawBase(canvas, ctx, "./meow_bongo_lower_right.png");
+
+  document.body.appendChild(canvas);
+}
+
+main();
